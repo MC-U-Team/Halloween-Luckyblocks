2995e953ab5b75be82cb14ef8c8080a0e4ae6d40
HyCraftHD <hycrafthd@live.de>
2019-10-26 23:47:04 +0200

    Fix render for vampire

--- src/main/java/info/u_team/halloween_luckyblock/entity/render/RenderVampire.java
+++ src/main/java/info/u_team/halloween_luckyblock/entity/render/RenderVampire.java
@@ -1,38 +1,39 @@
 package info.u_team.halloween_luckyblock.entity.render;
 
-import info.u_team.halloween_luckyblock.HalloweenLuckyBlockConstants;
+import com.mojang.blaze3d.platform.GlStateManager;
+
+import info.u_team.halloween_luckyblock.HalloweenLuckyBlockMod;
 import info.u_team.halloween_luckyblock.entity.EntityVampire;
 import info.u_team.halloween_luckyblock.entity.model.ModelVampire;
-import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.*;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.MathHelper;
-import net.minecraftforge.fml.relauncher.*;
+import net.minecraftforge.api.distmarker.*;
 
-@SideOnly(Side.CLIENT)
-public class RenderVampire extends RenderLiving<EntityVampire> {
+@OnlyIn(Dist.CLIENT)
+public class RenderVampire extends MobRenderer<EntityVampire, ModelVampire> {
 	
-	public RenderVampire(RenderManager rendermanager) {
+	public RenderVampire(EntityRendererManager rendermanager) {
 		super(rendermanager, new ModelVampire(), 0.25F);
 	}
 	
 	@Override
 	protected void preRenderCallback(EntityVampire entitylivingbaseIn, float partialTickTime) {
-		GlStateManager.scale(1.35F, 1.35F, 1.35F);
+		GlStateManager.scalef(1.35F, 1.35F, 1.35F);
 	}
 	
 	@Override
 	protected void applyRotations(EntityVampire entityLiving, float p_77043_2_, float rotationYaw, float partialTicks) {
 		if (!entityLiving.getIsBatHanging()) {
-			GlStateManager.translate(0.0F, MathHelper.cos(p_77043_2_ * 0.3F) * 0.1F, 0.0F);
+			GlStateManager.translatef(0.0F, MathHelper.cos(p_77043_2_ * 0.3F) * 0.1F, 0.0F);
 		} else {
-			GlStateManager.translate(0.0F, -0.1F, 0.0F);
+			GlStateManager.translatef(0.0F, -0.1F, 0.0F);
 		}
 		super.applyRotations(entityLiving, p_77043_2_, rotationYaw, partialTicks);
 	}
 	
 	@Override
 	protected ResourceLocation getEntityTexture(EntityVampire entity) {
-		return new ResourceLocation(HalloweenLuckyBlockConstants.MODID + ":textures/entity/vampire.png");
+		return new ResourceLocation(HalloweenLuckyBlockMod.MODID + ":textures/entity/vampire.png");
 	}
 }
