f48cbb62b858b11496a388ab5aa02d366241ed6a
HyCraftHD <hycrafthd@live.de>
2019-10-26 23:42:05 +0200

    Fix vampire model

--- src/main/java/info/u_team/halloween_luckyblock/entity/model/ModelVampire.java
+++ src/main/java/info/u_team/halloween_luckyblock/entity/model/ModelVampire.java
@@ -1,45 +1,44 @@
 package info.u_team.halloween_luckyblock.entity.model;
 
 import info.u_team.halloween_luckyblock.entity.EntityVampire;
-import net.minecraft.client.model.*;
-import net.minecraft.entity.Entity;
+import net.minecraft.client.renderer.entity.model.*;
 import net.minecraft.util.math.MathHelper;
-import net.minecraftforge.fml.relauncher.*;
+import net.minecraftforge.api.distmarker.*;
 
-@SideOnly(Side.CLIENT)
-public class ModelVampire extends ModelBase {
+@OnlyIn(Dist.CLIENT)
+public class ModelVampire extends EntityModel<EntityVampire> {
 	
-	private ModelRenderer batHead;
-	private ModelRenderer batBody;
-	private ModelRenderer batRightWing;
-	private ModelRenderer batLeftWing;
-	private ModelRenderer batOuterRightWing;
-	private ModelRenderer batOuterLeftWing;
+	private RendererModel batHead;
+	private RendererModel batBody;
+	private RendererModel batRightWing;
+	private RendererModel batLeftWing;
+	private RendererModel batOuterRightWing;
+	private RendererModel batOuterLeftWing;
 	
 	public ModelVampire() {
 		this.textureWidth = 64;
 		this.textureHeight = 64;
-		this.batHead = new ModelRenderer(this, 0, 0);
+		this.batHead = new RendererModel(this, 0, 0);
 		this.batHead.addBox(-3.0F, -3.0F, -3.0F, 6, 6, 6);
-		ModelRenderer modelrenderer = new ModelRenderer(this, 24, 0);
-		modelrenderer.addBox(-4.0F, -6.0F, -2.0F, 3, 4, 1);
-		this.batHead.addChild(modelrenderer);
-		ModelRenderer modelrenderer1 = new ModelRenderer(this, 24, 0);
-		modelrenderer1.mirror = true;
-		modelrenderer1.addBox(1.0F, -6.0F, -2.0F, 3, 4, 1);
-		this.batHead.addChild(modelrenderer1);
-		this.batBody = new ModelRenderer(this, 0, 16);
+		RendererModel RendererModel = new RendererModel(this, 24, 0);
+		RendererModel.addBox(-4.0F, -6.0F, -2.0F, 3, 4, 1);
+		this.batHead.addChild(RendererModel);
+		RendererModel RendererModel1 = new RendererModel(this, 24, 0);
+		RendererModel1.mirror = true;
+		RendererModel1.addBox(1.0F, -6.0F, -2.0F, 3, 4, 1);
+		this.batHead.addChild(RendererModel1);
+		this.batBody = new RendererModel(this, 0, 16);
 		this.batBody.addBox(-3.0F, 4.0F, -3.0F, 6, 12, 6);
 		this.batBody.setTextureOffset(0, 34).addBox(-5.0F, 16.0F, 0.0F, 10, 6, 1);
-		this.batRightWing = new ModelRenderer(this, 42, 0);
+		this.batRightWing = new RendererModel(this, 42, 0);
 		this.batRightWing.addBox(-12.0F, 1.0F, 1.5F, 10, 16, 1);
-		this.batOuterRightWing = new ModelRenderer(this, 24, 16);
+		this.batOuterRightWing = new RendererModel(this, 24, 16);
 		this.batOuterRightWing.setRotationPoint(-12.0F, 1.0F, 1.5F);
 		this.batOuterRightWing.addBox(-8.0F, 1.0F, 0.0F, 8, 12, 1);
-		this.batLeftWing = new ModelRenderer(this, 42, 0);
+		this.batLeftWing = new RendererModel(this, 42, 0);
 		this.batLeftWing.mirror = true;
 		this.batLeftWing.addBox(2.0F, 1.0F, 1.5F, 10, 16, 1);
-		this.batOuterLeftWing = new ModelRenderer(this, 24, 16);
+		this.batOuterLeftWing = new RendererModel(this, 24, 16);
 		this.batOuterLeftWing.mirror = true;
 		this.batOuterLeftWing.setRotationPoint(12.0F, 1.0F, 1.5F);
 		this.batOuterLeftWing.addBox(0.0F, 1.0F, 0.0F, 8, 12, 1);
@@ -50,14 +49,14 @@ public class ModelVampire extends ModelBase {
 	}
 	
 	@Override
-	public void render(Entity p_78088_1_, float p_78088_2_, float p_78088_3_, float p_78088_4_, float p_78088_5_, float p_78088_6_, float p_78088_7_) {
-		this.setRotationAngles(p_78088_2_, p_78088_3_, p_78088_4_, p_78088_5_, p_78088_6_, p_78088_7_, p_78088_1_);
+	public void render(EntityVampire p_78088_1_, float p_78088_2_, float p_78088_3_, float p_78088_4_, float p_78088_5_, float p_78088_6_, float p_78088_7_) {
+		this.setRotationAngles(p_78088_1_, p_78088_2_, p_78088_3_, p_78088_4_, p_78088_5_, p_78088_6_, p_78088_7_);
 		this.batHead.render(p_78088_7_);
 		this.batBody.render(p_78088_7_);
 	}
 	
 	@Override
-	public void setRotationAngles(float p_78087_1_, float p_78087_2_, float p_78087_3_, float p_78087_4_, float p_78087_5_, float p_78087_6_, Entity p_78087_7_) {
+	public void setRotationAngles(EntityVampire p_78087_7_, float p_78087_1_, float p_78087_2_, float p_78087_3_, float p_78087_4_, float p_78087_5_, float p_78087_6_) {
 		
 		if (((EntityVampire) p_78087_7_).getIsBatHanging()) {
 			this.batHead.rotateAngleX = p_78087_5_ / (180F / (float) Math.PI);
