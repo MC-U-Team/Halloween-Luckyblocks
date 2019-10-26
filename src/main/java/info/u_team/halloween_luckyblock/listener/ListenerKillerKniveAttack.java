8df25d5ef5f2727b002916a279487fc0b7f6a155
HyCraftHD <hycrafthd@live.de>
2019-10-26 17:54:56 +0200

    Fix listeners

--- src/main/java/info/u_team/halloween_luckyblock/listener/ListenerKillerKniveAttack.java
+++ src/main/java/info/u_team/halloween_luckyblock/listener/ListenerKillerKniveAttack.java
@@ -2,29 +2,29 @@ package info.u_team.halloween_luckyblock.listener;
 
 import info.u_team.halloween_luckyblock.init.HalloweenLuckyBlockItems;
 import net.minecraft.entity.*;
-import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.*;
-import net.minecraft.util.EnumHand;
+import net.minecraft.util.Hand;
 import net.minecraft.util.math.Vec3d;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class ListenerKillerKniveAttack {
 	
 	@SubscribeEvent
 	public static void on(LivingAttackEvent event) {
 		Entity entity = event.getSource().getImmediateSource();
-		if (entity == null || !(entity instanceof EntityPlayerMP)) {
+		if (entity == null || !(entity instanceof ServerPlayerEntity)) {
 			return;
 		}
-		EntityPlayerMP player = (EntityPlayerMP) entity;
+		ServerPlayerEntity player = (ServerPlayerEntity) entity;
 		ItemStack stack = player.inventory.getCurrentItem();
 		if (stack == null || stack.getItem() == null) {
 			return;
 		}
 		Item item = stack.getItem();
-		if (item == HalloweenLuckyBlockItems.killerknive) {
-			EntityLivingBase base = event.getEntityLiving();
+		if (item == HalloweenLuckyBlockItems.KILLERKNIFE) {
+			LivingEntity base = event.getEntityLiving();
 			
 			Vec3d lookplayer = player.getLookVec();
 			Vec3d lookbase = base.getLookVec();
@@ -34,10 +34,9 @@ public class ListenerKillerKniveAttack {
 			
 			if (differx < 0.4 && differz < 0.4) {
 				base.onKillCommand();
-				stack.damageItem(40, player);
-				if (stack.getItemDamage() >= stack.getMaxDamage() || stack.getMaxStackSize() == 0) {
-					player.setHeldItem(EnumHand.MAIN_HAND, ItemStack.EMPTY);
-				}
+				stack.damageItem(40, player, x -> {
+					x.setHeldItem(Hand.MAIN_HAND, ItemStack.EMPTY);
+				});
 			}
 		}
 	}
--- src/main/java/info/u_team/halloween_luckyblock/listener/ListenerZombieArmor.java
+++ src/main/java/info/u_team/halloween_luckyblock/listener/ListenerZombieArmor.java
@@ -3,27 +3,27 @@ package info.u_team.halloween_luckyblock.listener;
 import info.u_team.halloween_luckyblock.init.HalloweenLuckyBlockSounds;
 import info.u_team.halloween_luckyblock.util.ArmorUtility;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.*;
-import net.minecraft.potion.PotionEffect;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.potion.*;
+import net.minecraft.util.SoundEvents;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class ListenerZombieArmor {
 	
 	@SubscribeEvent
 	public static void on(LivingUpdateEvent event) {
 		Entity entity = event.getEntityLiving();
-		if (!(entity instanceof EntityPlayer)) {
+		if (!(entity instanceof PlayerEntity)) {
 			return;
 		}
-		EntityPlayer player = (EntityPlayer) entity;
-		if (player.capabilities.isCreativeMode) {
+		PlayerEntity player = (PlayerEntity) entity;
+		if (player.abilities.isCreativeMode) {
 			return;
 		}
 		int count = ArmorUtility.getArmorBaseCount(player, "zombie");
 		if (count > 0) {
-			player.addPotionEffect(new PotionEffect(MobEffects.SLOWNESS, 2, 0, false, false));
+			player.addPotionEffect(new EffectInstance(Effects.SLOWNESS, 2, 0, false, false));
 			if (player.world.rand.nextInt(500) == 0) {
 				player.world.playSound(null, player.getPosition(), SoundEvents.ENTITY_ZOMBIE_AMBIENT, HalloweenLuckyBlockSounds.category, 0.2F, 1.0F);
 			}
