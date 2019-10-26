09b7c889e17fe0e261e9effd99714580f2f7202c
HyCraftHD <hycrafthd@live.de>
2019-10-26 18:31:29 +0200

    Fix vampire entity

--- src/main/java/info/u_team/halloween_luckyblock/entity/EntityVampire.java
+++ src/main/java/info/u_team/halloween_luckyblock/entity/EntityVampire.java
@@ -1,36 +1,36 @@
 package info.u_team.halloween_luckyblock.entity;
 
 import info.u_team.halloween_luckyblock.init.*;
-import net.minecraft.block.state.IBlockState;
+import net.minecraft.block.BlockState;
 import net.minecraft.entity.*;
-import net.minecraft.entity.ai.*;
-import net.minecraft.entity.item.EntityFallingBlock;
-import net.minecraft.entity.passive.*;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.SoundEvents;
-import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.entity.ai.goal.*;
+import net.minecraft.entity.item.FallingBlockEntity;
+import net.minecraft.entity.passive.AmbientEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.network.datasync.*;
 import net.minecraft.util.*;
 import net.minecraft.util.math.*;
-import net.minecraft.world.World;
+import net.minecraft.world.*;
 
-public class EntityVampire extends EntityAmbientCreature {
+public class EntityVampire extends AmbientEntity {
 	
-	private static final DataParameter<Byte> HANGING = EntityDataManager.<Byte> createKey(EntityBat.class, DataSerializers.BYTE);
+	private static final DataParameter<Byte> HANGING = EntityDataManager.<Byte> createKey(EntityVampire.class, DataSerializers.BYTE);
+	private static final EntityPredicate field_213813_c = (new EntityPredicate()).setDistance(4.0D).allowFriendlyFire();
 	private BlockPos spawnPosition;
 	
-	public EntityVampire(World worldIn) {
-		super(worldIn);
-		this.setSize(2.0F, 2.0F);
+	public EntityVampire(EntityType<? extends EntityVampire> type, World world) {
+		super(type, world);
+		// this.setSize(2.0F, 2.0F); TODO
 		this.setIsBatHanging(true);
-		this.tasks.addTask(2, new EntityVampire.AIAttack());
-		this.targetTasks.addTask(1, new EntityAIFindEntityNearestPlayer(this));
+		this.goalSelector.addGoal(2, new AIAttack());
+		this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, PlayerEntity.class, 10, true, false, e -> true));
 	}
 	
 	@Override
-	public void entityInit() {
-		super.entityInit();
-		this.dataManager.register(HANGING, Byte.valueOf((byte) 0));
+	protected void registerData() {
+		super.registerData();
+		this.dataManager.register(HANGING, (byte) 0);
 	}
 	
 	@Override
@@ -71,11 +71,6 @@ public class EntityVampire extends EntityAmbientCreature {
 	protected void collideWithNearbyEntities() {
 	}
 	
-	@Override
-	protected void applyEntityAttributes() {
-		super.applyEntityAttributes();
-	}
-	
 	public boolean getIsBatHanging() {
 		return (this.dataManager.get(HANGING).byteValue() & 1) != 0;
 	}
@@ -91,16 +86,14 @@ public class EntityVampire extends EntityAmbientCreature {
 	}
 	
 	@Override
-	public void onUpdate() {
-		super.onUpdate();
+	public void tick() {
+		super.tick();
 		
 		if (this.getIsBatHanging()) {
-			this.motionX = 0.0D;
-			this.motionY = 0.0D;
-			this.motionZ = 0.0D;
-			this.posY = MathHelper.floor(this.posY) + 1.0D - this.height;
+			this.setMotion(Vec3d.ZERO);
+			this.posY = (double) MathHelper.floor(this.posY) + 1.0D - (double) this.getHeight();
 		} else {
-			this.motionY *= 0.6000000238418579D;
+			this.setMotion(this.getMotion().mul(1.0D, 0.6D, 1.0D));
 		}
 	}
 	
@@ -109,45 +102,44 @@ public class EntityVampire extends EntityAmbientCreature {
 		super.updateAITasks();
 		BlockPos blockpos = new BlockPos(this);
 		BlockPos blockpos1 = blockpos.up();
-		
 		if (this.getIsBatHanging()) {
-			if (this.world.getBlockState(blockpos1).isNormalCube()) {
+			if (this.world.getBlockState(blockpos1).isNormalCube(this.world, blockpos)) {
 				if (this.rand.nextInt(200) == 0) {
-					this.rotationYawHead = this.rand.nextInt(360);
+					this.rotationYawHead = (float) this.rand.nextInt(360);
 				}
 				
-				if (this.world.getNearestPlayerNotCreative(this, 4.0D) != null) {
+				if (this.world.getClosestPlayer(field_213813_c, this) != null) {
 					this.setIsBatHanging(false);
-					this.world.playEvent((EntityPlayer) null, 1025, blockpos, 0);
+					this.world.playEvent((PlayerEntity) null, 1025, blockpos, 0);
 				}
 			} else {
 				this.setIsBatHanging(false);
-				this.world.playEvent((EntityPlayer) null, 1025, blockpos, 0);
+				this.world.playEvent((PlayerEntity) null, 1025, blockpos, 0);
 			}
 		} else {
 			if (this.spawnPosition != null && (!this.world.isAirBlock(this.spawnPosition) || this.spawnPosition.getY() < 1)) {
 				this.spawnPosition = null;
 			}
 			
-			if (this.spawnPosition == null || this.rand.nextInt(30) == 0 || this.spawnPosition.distanceSq(((int) this.posX), ((int) this.posY), ((int) this.posZ)) < 4.0D) {
-				this.spawnPosition = new BlockPos((int) this.posX + this.rand.nextInt(7) - this.rand.nextInt(7), (int) this.posY + this.rand.nextInt(6) - 2, (int) this.posZ + this.rand.nextInt(7) - this.rand.nextInt(7));
+			if (this.spawnPosition == null || this.rand.nextInt(30) == 0 || this.spawnPosition.withinDistance(this.getPositionVec(), 2.0D)) {
+				this.spawnPosition = new BlockPos(this.posX + (double) this.rand.nextInt(7) - (double) this.rand.nextInt(7), this.posY + (double) this.rand.nextInt(6) - 2.0D, this.posZ + (double) this.rand.nextInt(7) - (double) this.rand.nextInt(7));
 			}
 			
-			double d0 = this.spawnPosition.getX() + 0.5D - this.posX;
-			double d1 = this.spawnPosition.getY() + 0.1D - this.posY;
-			double d2 = this.spawnPosition.getZ() + 0.5D - this.posZ;
-			this.motionX += (Math.signum(d0) * 0.5D - this.motionX) * 0.10000000149011612D;
-			this.motionY += (Math.signum(d1) * 0.699999988079071D - this.motionY) * 0.10000000149011612D;
-			this.motionZ += (Math.signum(d2) * 0.5D - this.motionZ) * 0.10000000149011612D;
-			float f = (float) (MathHelper.atan2(this.motionZ, this.motionX) * (180D / Math.PI)) - 90.0F;
+			double d0 = (double) this.spawnPosition.getX() + 0.5D - this.posX;
+			double d1 = (double) this.spawnPosition.getY() + 0.1D - this.posY;
+			double d2 = (double) this.spawnPosition.getZ() + 0.5D - this.posZ;
+			Vec3d vec3d = this.getMotion();
+			Vec3d vec3d1 = vec3d.add((Math.signum(d0) * 0.5D - vec3d.x) * (double) 0.1F, (Math.signum(d1) * (double) 0.7F - vec3d.y) * (double) 0.1F, (Math.signum(d2) * 0.5D - vec3d.z) * (double) 0.1F);
+			this.setMotion(vec3d1);
+			float f = (float) (MathHelper.atan2(vec3d1.z, vec3d1.x) * (double) (180F / (float) Math.PI)) - 90.0F;
 			float f1 = MathHelper.wrapDegrees(f - this.rotationYaw);
 			this.moveForward = 0.5F;
 			this.rotationYaw += f1;
-			
-			if (this.rand.nextInt(100) == 0 && this.world.getBlockState(blockpos1).isNormalCube()) {
+			if (this.rand.nextInt(100) == 0 && this.world.getBlockState(blockpos1).isNormalCube(this.world, blockpos1)) {
 				this.setIsBatHanging(true);
 			}
 		}
+		
 	}
 	
 	@Override
@@ -160,7 +152,7 @@ public class EntityVampire extends EntityAmbientCreature {
 	}
 	
 	@Override
-	protected void updateFallState(double y, boolean onGroundIn, IBlockState state, BlockPos pos) {
+	protected void updateFallState(double y, boolean onGroundIn, BlockState state, BlockPos pos) {
 	}
 	
 	@Override
@@ -170,7 +162,7 @@ public class EntityVampire extends EntityAmbientCreature {
 	
 	@Override
 	public boolean attackEntityFrom(DamageSource source, float amount) {
-		if (this.isEntityInvulnerable(source)) {
+		if (this.isInvulnerableTo(source)) {
 			return false;
 		} else {
 			if (!this.world.isRemote && this.getIsBatHanging()) {
@@ -182,36 +174,34 @@ public class EntityVampire extends EntityAmbientCreature {
 	}
 	
 	@Override
-	public void readEntityFromNBT(NBTTagCompound compound) {
-		super.readEntityFromNBT(compound);
-		this.dataManager.set(HANGING, Byte.valueOf(compound.getByte("BatFlags")));
+	public void readAdditional(CompoundNBT compound) {
+		super.readAdditional(compound);
+		this.dataManager.set(HANGING, compound.getByte("BatFlags"));
 	}
 	
 	@Override
-	public void writeEntityToNBT(NBTTagCompound compound) {
-		super.writeEntityToNBT(compound);
-		compound.setByte("BatFlags", this.dataManager.get(HANGING).byteValue());
+	public void writeAdditional(CompoundNBT compound) {
+		super.writeAdditional(compound);
+		compound.putByte("BatFlags", this.dataManager.get(HANGING));
 	}
 	
 	@Override
-	public boolean getCanSpawnHere() {
-		BlockPos blockpos = new BlockPos(this.posX, this.getEntityBoundingBox().minY, this.posZ);
-		
+	public boolean canSpawn(IWorld worldIn, SpawnReason spawnReasonIn) {
+		BlockPos blockpos = new BlockPos(this.posX, this.getBoundingBox().minY, this.posZ);
 		if (blockpos.getY() >= this.world.getSeaLevel()) {
 			return false;
 		} else {
-			int i = this.world.getLightFromNeighbors(blockpos);
+			int i = this.world.getLight(blockpos);
 			int j = 7;
-			return i > this.rand.nextInt(j) ? false : super.getCanSpawnHere();
+			return i > this.rand.nextInt(j) ? false : super.canSpawn(worldIn, spawnReasonIn);
 		}
 	}
 	
-	@Override
-	public float getEyeHeight() {
-		return this.height / 2.0F;
+	protected float getStandingEyeHeight(Pose poseIn, EntitySize sizeIn) {
+		return sizeIn.height / 2.0F;
 	}
 	
-	public class AIAttack extends EntityAIBase {
+	public class AIAttack extends Goal {
 		
 		private EntityVampire vampire = EntityVampire.this;
 		public int ticks;
@@ -231,8 +221,8 @@ public class EntityVampire extends EntityAmbientCreature {
 		}
 		
 		@Override
-		public void updateTask() {
-			EntityLivingBase entitylivingbase = this.vampire.getAttackTarget();
+		public void tick() {
+			LivingEntity entitylivingbase = this.vampire.getAttackTarget();
 			double d0 = 64.0D;
 			
 			if (entitylivingbase.getDistanceSq(this.vampire) < d0 * d0 && this.vampire.canEntityBeSeen(entitylivingbase)) {
@@ -241,11 +231,11 @@ public class EntityVampire extends EntityAmbientCreature {
 				++this.ticks;
 				if (this.ticks == 390) {
 					world.playSound(null, vampire.getPosition(), HalloweenLuckyBlockSounds.happy_halloween, SoundCategory.NEUTRAL, 1.0F, ((world.rand.nextFloat() * 0.8F) + 0.6F));
-					EntityFallingBlock falling = new EntityFallingBlock(world, entitylivingbase.posX, entitylivingbase.posY + 5, entitylivingbase.posZ, HalloweenLuckyBlockBlocks.pumpkinbomb.getDefaultState());
+					FallingBlockEntity falling = new FallingBlockEntity(world, entitylivingbase.posX, entitylivingbase.posY + 5, entitylivingbase.posZ, HalloweenLuckyBlockBlocks.PUMPKINBOMB.getDefaultState());
 					falling.fallTime = 100;
 					falling.shouldDropItem = false;
 					falling.setHurtEntities(false);
-					world.spawnEntity(falling);
+					world.addEntity(falling);
 					vampire.damageEntity(DamageSource.causeMobDamage(entitylivingbase), 4.0F);
 					this.ticks = -10;
 				}
