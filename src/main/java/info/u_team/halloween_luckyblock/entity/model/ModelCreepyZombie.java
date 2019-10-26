aa5a1812b104c9db0caae651cc902d9fd9f23d5f
HyCraftHD <hycrafthd@live.de>
2019-10-26 23:39:23 +0200

    Fix zombie model

--- src/main/java/info/u_team/halloween_luckyblock/entity/model/ModelCreepyZombie.java
+++ src/main/java/info/u_team/halloween_luckyblock/entity/model/ModelCreepyZombie.java
@@ -1,70 +1,70 @@
 package info.u_team.halloween_luckyblock.entity.model;
 
-import net.minecraft.client.model.*;
-import net.minecraft.entity.Entity;
+import info.u_team.halloween_luckyblock.entity.EntityCreepyZombie;
+import net.minecraft.client.renderer.entity.model.*;
 import net.minecraft.util.math.MathHelper;
-import net.minecraftforge.fml.relauncher.*;
+import net.minecraftforge.api.distmarker.*;
 
-@SideOnly(Side.CLIENT)
-public class ModelCreepyZombie extends ModelBase {
+@OnlyIn(Dist.CLIENT)
+public class ModelCreepyZombie extends EntityModel<EntityCreepyZombie> {
 	
-	private ModelRenderer headright;
-	private ModelRenderer headleft;
-	private ModelRenderer brain;
-	private ModelRenderer body;
-	private ModelRenderer rightarm;
-	private ModelRenderer leftarm;
-	private ModelRenderer rightleg;
-	private ModelRenderer leftleg;
+	private RendererModel headright;
+	private RendererModel headleft;
+	private RendererModel brain;
+	private RendererModel body;
+	private RendererModel rightarm;
+	private RendererModel leftarm;
+	private RendererModel rightleg;
+	private RendererModel leftleg;
 	
 	public ModelCreepyZombie() {
 		textureWidth = 64;
 		textureHeight = 32;
 		
-		headright = new ModelRenderer(this, 24, 0);
+		headright = new RendererModel(this, 24, 0);
 		headright.addBox(0F, -8F, -4F, 4, 8, 8);
 		headright.setRotationPoint(0F, 0F, 0F);
 		headright.setTextureSize(64, 32);
 		headright.mirror = true;
 		setRotation(headright, 0F, 0F, 0.3141593F);
-		headleft = new ModelRenderer(this, 0, 0);
+		headleft = new RendererModel(this, 0, 0);
 		headleft.addBox(-4F, -8F, -4F, 4, 8, 8);
 		headleft.setRotationPoint(0F, 0F, 0F);
 		headleft.setTextureSize(64, 32);
 		headleft.mirror = true;
 		setRotation(headleft, 0F, 0F, -0.1745329F);
-		brain = new ModelRenderer(this, 48, 0);
+		brain = new RendererModel(this, 48, 0);
 		brain.addBox(-2F, -6F, -2F, 4, 6, 4);
 		brain.setRotationPoint(0F, 0F, 0F);
 		brain.setTextureSize(64, 32);
 		brain.mirror = true;
 		setRotation(brain, 0F, 0F, 0F);
 		
-		body = new ModelRenderer(this, 16, 16);
+		body = new RendererModel(this, 16, 16);
 		body.addBox(-4F, 0F, -2F, 8, 12, 4);
 		body.setRotationPoint(0F, 0F, 0F);
 		body.setTextureSize(64, 32);
 		body.mirror = true;
 		setRotation(body, 0F, 0F, 0F);
-		rightarm = new ModelRenderer(this, 40, 16);
+		rightarm = new RendererModel(this, 40, 16);
 		rightarm.addBox(-3F, -2F, -2F, 4, 12, 4);
 		rightarm.setRotationPoint(-5F, 2F, 0F);
 		rightarm.setTextureSize(64, 32);
 		rightarm.mirror = true;
 		setRotation(rightarm, 0F, 0F, 0F);
-		leftarm = new ModelRenderer(this, 40, 16);
+		leftarm = new RendererModel(this, 40, 16);
 		leftarm.addBox(-1F, -2F, -2F, 4, 12, 4);
 		leftarm.setRotationPoint(5F, 2F, 0F);
 		leftarm.setTextureSize(64, 32);
 		leftarm.mirror = true;
 		setRotation(leftarm, 0F, 0F, 0F);
-		rightleg = new ModelRenderer(this, 0, 16);
+		rightleg = new RendererModel(this, 0, 16);
 		rightleg.addBox(-2F, 0F, -2F, 4, 12, 4);
 		rightleg.setRotationPoint(-2F, 12F, 0F);
 		rightleg.setTextureSize(64, 32);
 		rightleg.mirror = true;
 		setRotation(rightleg, 0F, 0F, 0F);
-		leftleg = new ModelRenderer(this, 0, 16);
+		leftleg = new RendererModel(this, 0, 16);
 		leftleg.addBox(-2F, 0F, -2F, 4, 12, 4);
 		leftleg.setRotationPoint(2F, 12F, 0F);
 		leftleg.setTextureSize(64, 32);
@@ -72,16 +72,16 @@ public class ModelCreepyZombie extends ModelBase {
 		setRotation(leftleg, 0F, 0F, 0F);
 	}
 	
-	private void setRotation(ModelRenderer model, float x, float y, float z) {
+	private void setRotation(RendererModel model, float x, float y, float z) {
 		model.rotateAngleX = x;
 		model.rotateAngleY = y;
 		model.rotateAngleZ = z;
 	}
 	
 	@Override
-	public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {
+	public void render(EntityCreepyZombie entity, float f, float f1, float f2, float f3, float f4, float f5) {
 		super.render(entity, f, f1, f2, f3, f4, f5);
-		setRotationAngles(f, f1, f2, f3, f4, f5, entity);
+		setRotationAngles(entity, f, f1, f2, f3, f4, f5);
 		headright.render(f5);
 		headleft.render(f5);
 		brain.render(f5);
@@ -93,8 +93,8 @@ public class ModelCreepyZombie extends ModelBase {
 	}
 	
 	@Override
-	public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5, Entity entity) {
-		super.setRotationAngles(f, f1, f2, f3, f4, f5, entity);
+	public void setRotationAngles(EntityCreepyZombie entity, float f, float f1, float f2, float f3, float f4, float f5) {
+		super.setRotationAngles(entity, f, f1, f2, f3, f4, f5);
 		
 		float f6 = MathHelper.sin(this.swingProgress * (float) Math.PI);
 		float f7 = MathHelper.sin((1.0F - (1.0F - this.swingProgress) * (1.0F - this.swingProgress)) * (float) Math.PI);
