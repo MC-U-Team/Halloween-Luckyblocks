cf2d9efa6b85ad1d01e0869bfd37a01ef47c8428
HyCraftHD <hycrafthd@live.de>
2019-10-27 00:36:22 +0200

    Remove old render registry

--- src/main/java/info/u_team/halloween_luckyblock/init/HalloweenLuckyBlockRenderRegistry.java
+++ src/main/java/info/u_team/halloween_luckyblock/init/HalloweenLuckyBlockRenderRegistry.java
@@ -4,6 +4,8 @@ import info.u_team.halloween_luckyblock.HalloweenLuckyBlockMod;
 import info.u_team.halloween_luckyblock.entity.*;
 import info.u_team.halloween_luckyblock.entity.render.*;
 import info.u_team.u_team_core.util.registry.ClientRegistry;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.entity.SpriteRenderer;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
@@ -18,6 +20,9 @@ public class HalloweenLuckyBlockRenderRegistry {
 		ClientRegistry.registerEntityRenderer(EntityCreepyZombie.class, RenderCreepyZombie::new);
 		ClientRegistry.registerEntityRenderer(EntityGhost.class, RenderGhost::new);
 		ClientRegistry.registerEntityRenderer(EntityVampire.class, RenderVampire::new);
+		ClientRegistry.registerEntityRenderer(EntityPumpkinGrenade.class, manager -> {
+			return new SpriteRenderer<>(manager, Minecraft.getInstance().getItemRenderer());
+		});
 	}
 	
 }
