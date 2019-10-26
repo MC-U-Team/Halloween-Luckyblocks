aed6072d421dfa93d94c9d0ff6dfda26e779846f
HyCraftHD <hycrafthd@live.de>
2019-10-27 00:25:49 +0200

    Fix pumpkin bomb render issues

--- src/main/java/info/u_team/halloween_luckyblock/block/BlockPumpkinBomb.java
+++ src/main/java/info/u_team/halloween_luckyblock/block/BlockPumpkinBomb.java
@@ -8,10 +8,13 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.*;
 import net.minecraft.util.text.TranslationTextComponent;
-import net.minecraft.world.World;
+import net.minecraft.world.*;
 
 public class BlockPumpkinBomb extends UTileEntityBlock {
+	
+	private static final VoxelShape SHAPE = makeCuboidShape(4, 4, 4, 12, 12, 12);
 	
 	public BlockPumpkinBomb(String name) {
 		super(name, HalloweenLuckyBlockItemGroups.GROUP, Properties.create(Material.ROCK).hardnessAndResistance(-1, 100000).noDrops(), () -> HalloweenLuckyBlockTileEntityTypes.PUMPKINBOMB);
@@ -22,5 +25,20 @@ public class BlockPumpkinBomb extends UTileEntityBlock {
 		if (placer instanceof PlayerEntity && !world.isRemote) {
 			placer.sendMessage(new TranslationTextComponent("tile.pumpkinbomb.place"));
 		}
+	}
+	
+	@Override
+	public boolean isNormalCube(BlockState state, IBlockReader worldIn, BlockPos pos) {
+		return false;
+	}
+	
+	@Override
+	public boolean isSolid(BlockState state) {
+		return false;
+	}
+	
+	@Override
+	public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+		return SHAPE;
 	}
 }
