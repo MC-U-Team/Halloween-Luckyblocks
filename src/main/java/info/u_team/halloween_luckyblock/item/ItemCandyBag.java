06d28764fc2f19e8e4fce83527f93fb2d5cf774b
HyCraftHD <hycrafthd@live.de>
2019-10-26 16:05:03 +0200

    Fix food item

--- src/main/java/info/u_team/halloween_luckyblock/item/ItemCandyBag.java
+++ src/main/java/info/u_team/halloween_luckyblock/item/ItemCandyBag.java
@@ -1,64 +1,41 @@
 package info.u_team.halloween_luckyblock.item;
 
 import info.u_team.halloween_luckyblock.init.HalloweenLuckyBlockItemGroups;
-import info.u_team.u_team_core.item.UItemFood;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.MobEffects;
+import info.u_team.u_team_core.item.UItem;
 import net.minecraft.item.*;
-import net.minecraft.potion.PotionEffect;
-import net.minecraft.util.NonNullList;
-import net.minecraft.world.World;
-import net.minecraftforge.fml.relauncher.*;
+import net.minecraft.potion.*;
+import net.minecraftforge.api.distmarker.*;
 
-public class ItemCandyBag extends UItemFood {
+public class ItemCandyBag extends UItem {
 	
-	public ItemCandyBag(String name) {
-		super(name, HalloweenLuckyBlockItemGroups.GROUP, 4, 2.0F);
-		setAlwaysEdible();
-		setHasSubtypes(true);
+	private static final Food normalFood = new Food.Builder() //
+			.setAlwaysEdible() //
+			.hunger(4) //
+			.saturation(2) //
+			.effect(new EffectInstance(Effects.ABSORPTION, 1200, 1), 1) //
+			.build();
+	
+	private static final Food betterFood = new Food.Builder() //
+			.setAlwaysEdible() //
+			.hunger(4) //
+			.saturation(10) //
+			.effect(new EffectInstance(Effects.ABSORPTION, 2400, 4), 1) //
+			.effect(new EffectInstance(Effects.REGENERATION, 1200, 1), 1) //
+			.effect(new EffectInstance(Effects.RESISTANCE, 600, 2), 0.9F) //
+			.effect(new EffectInstance(Effects.SPEED, 1200, 0), 1) //
+			.effect(new EffectInstance(Effects.JUMP_BOOST, 1200, 0), 1) //
+			.build();
+	
+	private final boolean betterItem;
+	
+	public ItemCandyBag(String name, boolean betterItem) {
+		super(name, HalloweenLuckyBlockItemGroups.GROUP, new Properties().food(betterItem ? betterFood : normalFood).rarity(betterItem ? Rarity.EPIC : Rarity.RARE));
+		this.betterItem = betterItem;
 	}
 	
-	@SideOnly(Side.CLIENT)
+	@OnlyIn(Dist.CLIENT)
 	@Override
 	public boolean hasEffect(ItemStack stack) {
-		return stack.getMetadata() > 0;
-	}
-	
-	@SideOnly(Side.CLIENT)
-	@Override
-	public void getSubItems(CreativeTabs tab, NonNullList<ItemStack> items) {
-		if (this.isInCreativeTab(tab)) {
-			items.add(new ItemStack(this, 1, 0));
-			items.add(new ItemStack(this, 1, 1));
-		}
-		
-	}
-	
-	@SideOnly(Side.CLIENT)
-	@Override
-	public void registerModel() {
-		super.registerModel();
-		setModel(this, 1, getRegistryName());
-	}
-	
-	@Override
-	public EnumRarity getRarity(ItemStack stack) {
-		return stack.getMetadata() == 0 ? EnumRarity.RARE : EnumRarity.EPIC;
-	}
-	
-	@Override
-	public void onFoodEaten(ItemStack stack, World worldIn, EntityPlayer player) {
-		if (!worldIn.isRemote) {
-			if (stack.getMetadata() == 0) {
-				player.addPotionEffect(new PotionEffect(MobEffects.ABSORPTION, 1200, 1));
-			} else {
-				player.addPotionEffect(new PotionEffect(MobEffects.ABSORPTION, 2400, 4));
-				player.addPotionEffect(new PotionEffect(MobEffects.REGENERATION, 1200, 1));
-				player.addPotionEffect(new PotionEffect(MobEffects.RESISTANCE, 600, 2));
-				player.addPotionEffect(new PotionEffect(MobEffects.SPEED, 1200, 0));
-				player.addPotionEffect(new PotionEffect(MobEffects.JUMP_BOOST, 1200, 0));
-			}
-		}
+		return betterItem;
 	}
 }
