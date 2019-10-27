4f24da40e6ed898e9d48e1eefa5adf11cc015824
HyCraftHD <hycrafthd@live.de>
2019-10-27 02:03:41 +0200

    Fix icon in item group

--- src/main/java/info/u_team/halloween_luckyblock/init/HalloweenLuckyBlockItemGroups.java
+++ src/main/java/info/u_team/halloween_luckyblock/init/HalloweenLuckyBlockItemGroups.java
@@ -2,10 +2,9 @@ package info.u_team.halloween_luckyblock.init;
 
 import info.u_team.halloween_luckyblock.HalloweenLuckyBlockMod;
 import info.u_team.u_team_core.itemgroup.UItemGroup;
-import net.minecraft.item.Items;
 
 public class HalloweenLuckyBlockItemGroups {
 	
-	public static final UItemGroup GROUP = new UItemGroup(HalloweenLuckyBlockMod.MODID, "group", () -> Items.ACACIA_BOAT); //TODO remove placeholder
+	public static final UItemGroup GROUP = new UItemGroup(HalloweenLuckyBlockMod.MODID, "group", () -> HalloweenLuckyBlockBlocks.LUCKYBLOCK);
 	
 }
