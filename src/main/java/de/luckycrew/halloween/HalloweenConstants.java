b811a792c65f1424c5dc27be9f3b30367d58bc5f
HyCraftHD <hycrafthd@live.de>
2020-10-23 16:17:29 +0200

    Fix update url

--- src/main/java/de/luckycrew/halloween/HalloweenConstants.java
+++ src/main/java/de/luckycrew/halloween/HalloweenConstants.java
@@ -5,21 +5,21 @@ import org.apache.logging.log4j.*;
 import net.minecraftforge.common.ForgeVersion;
 
 public class HalloweenConstants {
-	
+
 	public static final String MODID = "luckyblock_halloween";
 	public static final String NAME = "Halloween LuckyBlocks";
 	public static final String VERSION = "@VERSION@";
 	public static final String MCVERSION = ForgeVersion.mcVersion;
 	public static final String DEPENDENCIES = "required-after:uteamcore@[1.3.0,);";
-	
-	public static final String UPDATEURL = "https://api.u-team.info/update/luckyblocks/halloween.json";
-	
+
+	public static final String UPDATEURL = "https://api.u-team.info/update/halloween_luckyblock.json";
+
 	public static final String COMMONPROXY = "de.luckycrew.halloween.proxy.CommonProxy";
 	public static final String CLIENTPROXY = "de.luckycrew.halloween.proxy.ClientProxy";
-	
+
 	public static final Logger LOGGER = LogManager.getLogger(NAME);
-	
+
 	private HalloweenConstants() {
 	}
-	
+
 }
