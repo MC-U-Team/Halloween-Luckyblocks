e421bc239a599cbd8bf56a3598e291d28a9c6bb7
HyCraftHD <hycrafthd@live.de>
2020-10-23 16:47:06 +0200

    Probably dont need them anymore

--- src/main/java/de/luckycrew/halloween/entity/HalloweenEntities.java
+++ src/main/java/de/luckycrew/halloween/entity/HalloweenEntities.java
@@ -10,16 +10,16 @@ public class HalloweenEntities {
 	}
 	
 	private void throwable() {
-		EntityRegistry.registerGlobalEntityID(EntityPumpkinGrenade.class, "pumpkingrenade", EntityRegistry.findGlobalUniqueEntityId());
+//		EntityRegistry.registerGlobalEntityID(EntityPumpkinGrenade.class, "pumpkingrenade", EntityRegistry.findGlobalUniqueEntityId());
 		EntityRegistry.registerModEntity(EntityPumpkinGrenade.class, "pumpkingrenade", 0, USub.getID(), 64, 1, true);
 		
-		EntityRegistry.registerGlobalEntityID(EntityCreepyZombie.class, "creepyzombie", EntityRegistry.findGlobalUniqueEntityId());
+//		EntityRegistry.registerGlobalEntityID(EntityCreepyZombie.class, "creepyzombie", EntityRegistry.findGlobalUniqueEntityId());
 		EntityRegistry.registerModEntity(EntityCreepyZombie.class, "creepyzombie", 1, USub.getID(), 64, 3, true, 0x2ecc71, 0xe67e22);
 		
-		EntityRegistry.registerGlobalEntityID(EntityGhost.class, "ghost", EntityRegistry.findGlobalUniqueEntityId());
+//		EntityRegistry.registerGlobalEntityID(EntityGhost.class, "ghost", EntityRegistry.findGlobalUniqueEntityId());
 		EntityRegistry.registerModEntity(EntityGhost.class, "ghost", 2, USub.getID(), 64, 3, true, 0x797a74, 0xffffff);
 		
-		EntityRegistry.registerGlobalEntityID(EntityVampire.class, "vampire", EntityRegistry.findGlobalUniqueEntityId());
+//		EntityRegistry.registerGlobalEntityID(EntityVampire.class, "vampire", EntityRegistry.findGlobalUniqueEntityId());
 		EntityRegistry.registerModEntity(EntityVampire.class, "vampire", 3, USub.getID(), 64, 3, true, 0x686c72, 0x000000);
 	}
 	
