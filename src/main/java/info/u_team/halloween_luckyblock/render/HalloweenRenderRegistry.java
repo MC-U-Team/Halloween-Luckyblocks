7ae2aeb8273cc1f23669f923a11e35811489974a
HyCraftHD <hycrafthd@live.de>
2020-10-24 12:03:21 +0200

    Use lambda

--- src/main/java/info/u_team/halloween_luckyblock/render/HalloweenRenderRegistry.java
+++ src/main/java/info/u_team/halloween_luckyblock/render/HalloweenRenderRegistry.java
@@ -5,20 +5,12 @@ import info.u_team.halloween_luckyblock.entity.render.*;
 import info.u_team.halloween_luckyblock.init.HalloweenLuckyBlockItems;
 import info.u_team.u_team_core.registry.ClientRegistry;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.entity.*;
-import net.minecraftforge.fml.client.registry.IRenderFactory;
+import net.minecraft.client.renderer.entity.RenderSnowball;
 
 public class HalloweenRenderRegistry {
 	
 	public static void preinit() {
-		ClientRegistry.registerEntityRenderer(EntityPumpkinGrenade.class, new IRenderFactory<EntityPumpkinGrenade>() {
-			
-			@Override
-			public Render<EntityPumpkinGrenade> createRenderFor(RenderManager manager) {
-				return new RenderSnowball<>(manager, HalloweenLuckyBlockItems.pumpkingrenade, Minecraft.getMinecraft().getRenderItem());
-			}
-		});
-		
+		ClientRegistry.registerEntityRenderer(EntityPumpkinGrenade.class, manager -> new RenderSnowball<>(manager, HalloweenLuckyBlockItems.pumpkingrenade, Minecraft.getMinecraft().getRenderItem()));
 		ClientRegistry.registerEntityRenderer(EntityCreepyZombie.class, RenderCreepyZombie::new);
 		ClientRegistry.registerEntityRenderer(EntityGhost.class, RenderGhost::new);
 		ClientRegistry.registerEntityRenderer(EntityVampire.class, RenderVampire::new);
