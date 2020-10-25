320f105087c6d7440b132a20ca3fa6a6cc641668
HyCraftHD <hycrafthd@live.de>
2020-10-25 17:53:21 +0100

    Update tile entity pumpkin bomb

--- src/main/java/info/u_team/halloween_luckyblock/tileentity/TileEntityPumpkinBomb.java
+++ src/main/java/info/u_team/halloween_luckyblock/tileentity/TileEntityPumpkinBomb.java
@@ -1,18 +1,18 @@
 package info.u_team.halloween_luckyblock.tileentity;
 
 import info.u_team.halloween_luckyblock.init.HalloweenLuckyBlockTileEntityTypes;
-import info.u_team.halloween_luckyblock.util.*;
-import info.u_team.u_team_core.tileentity.UTileEntity;
-import net.minecraft.block.Blocks;
+import info.u_team.halloween_luckyblock.util.PumpkinUtil;
+import info.u_team.u_team_core.tileentity.UTickableTileEntity;
+import info.u_team.u_team_core.util.MathUtil;
+import net.minecraft.block.*;
 import net.minecraft.entity.item.FallingBlockEntity;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.particles.ParticleTypes;
-import net.minecraft.tileentity.ITickableTileEntity;
 import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.Explosion;
 
-public class TileEntityPumpkinBomb extends UTileEntity implements ITickableTileEntity {
+public class TileEntityPumpkinBomb extends UTickableTileEntity {
 	
 	private int fuse = 120;
 	
@@ -26,39 +26,40 @@ public class TileEntityPumpkinBomb extends UTileEntity implements ITickableTileE
 	}
 	
 	@Override
-	public void readNBT(CompoundNBT compound) {
+	public void readNBT(BlockState state, CompoundNBT compound) {
 		fuse = compound.getInt("fuse");
 	}
 	
 	@Override
-	public void tick() {
+	protected void tickServer() {
 		if (fuse <= 0) {
-			if (!world.isRemote) {
-				explode();
-			}
-		} else {
-			world.addParticle(ParticleTypes.LARGE_SMOKE, pos.getX() + 0.5D, pos.getY() + 0.9D, pos.getZ() + 0.5D, 0.0D, 0.2D, 0.0D);
-			if (fuse % 20 == 0) {
-				world.playSound(null, pos, SoundEvents.BLOCK_FIRE_EXTINGUISH, SoundCategory.BLOCKS, 0.8F, 0.4F / (world.rand.nextFloat() * 2F + 4F));
-			}
-			fuse--;
+			explode();
 		}
-		
+		fuse--;
+	}
+	
+	@Override
+	protected void tickClient() {
+		world.addParticle(ParticleTypes.LARGE_SMOKE, pos.getX() + 0.5, pos.getY() + 0.9, pos.getZ() + 0.5, 0, 0.2, 0);
+		if (fuse % 20 == 0) {
+			world.playSound(null, pos, SoundEvents.BLOCK_FIRE_EXTINGUISH, SoundCategory.BLOCKS, 0.8F, 0.4F / (world.rand.nextFloat() * 2 + 4));
+		}
+		fuse--;
 	}
 	
 	private void explode() {
 		world.setBlockState(pos, Blocks.AIR.getDefaultState());
+		
 		world.createExplosion(null, pos.getX(), pos.getY(), pos.getZ(), 8, true, Explosion.Mode.BREAK);
 		
-		BlockPos highpos = new BlockPos(pos.getX(), pos.getY() + 5, pos.getZ());
-		for (int i = 0; i < 35; i++) {
-			BlockPos newpos = highpos.add(MathUtil.getRandomNumberInRange(-8, 8), MathUtil.getRandomNumberInRange(1, 10), MathUtil.getRandomNumberInRange(-8, 8));
-			FallingBlockEntity falling = new FallingBlockEntity(world, newpos.getX() + 0.5, newpos.getY(), newpos.getZ() + 0.5, PumpkinUtil.getRandomPumpkin());
+		final BlockPos highPos = new BlockPos(pos.getX(), pos.getY() + 5, pos.getZ());
+		for (int index = 0; index < 35; index++) {
+			final BlockPos dropPos = highPos.add(MathUtil.randomNumberInRange(-8, 8), MathUtil.randomNumberInRange(1, 10), MathUtil.randomNumberInRange(-8, 8));
+			final FallingBlockEntity falling = new FallingBlockEntity(world, dropPos.getX() + 0.5, dropPos.getY(), dropPos.getZ() + 0.5, PumpkinUtil.getRandomPumpkin());
 			falling.fallTime = 100;
 			falling.shouldDropItem = false;
 			falling.setHurtEntities(true);
 			world.addEntity(falling);
 		}
 	}
-	
 }
