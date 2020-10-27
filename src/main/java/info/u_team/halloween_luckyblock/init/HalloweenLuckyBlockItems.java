53d53c3adcf4756c0fbebd2fb9d85bb87dd0bc26
HyCraftHD <hycrafthd@live.de>
2020-10-27 23:45:07 +0100

    Register items now + lazy spawnegg map wip

--- src/main/java/info/u_team/halloween_luckyblock/init/HalloweenLuckyBlockItems.java
+++ src/main/java/info/u_team/halloween_luckyblock/init/HalloweenLuckyBlockItems.java
@@ -2,41 +2,40 @@ package info.u_team.halloween_luckyblock.init;
 
 import info.u_team.halloween_luckyblock.HalloweenLuckyBlockMod;
 import info.u_team.halloween_luckyblock.item.*;
-import info.u_team.halloween_luckyblock.util.USpawnEggItem;
+import info.u_team.halloween_luckyblock.util.LazySpawnEggItem;
 import info.u_team.u_team_core.item.armor.*;
-import info.u_team.u_team_core.util.registry.BaseRegistryUtil;
+import info.u_team.u_team_core.util.registry.CommonDeferredRegister;
 import net.minecraft.item.*;
 import net.minecraft.item.Item.Properties;
-import net.minecraftforge.event.RegistryEvent.Register;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.ForgeRegistries;
 
-@EventBusSubscriber(modid = HalloweenLuckyBlockMod.MODID, bus = Bus.MOD)
 public class HalloweenLuckyBlockItems {
 	
-	public static final Item CANDY = new ItemCandy("candy");
-	public static final Item TELEPORTER = new ItemTeleporter("teleporter");
-	public static final Item CANDYBAG = new ItemCandyBag("candybag", false);
-	public static final Item CANDYBAG_OP = new ItemCandyBag("candybag_op", true);
-	public static final Item WITCHBROOMSTICK = new ItemWitchsBroomstick("witchsbroomstick");
-	public static final Item PUMPKINGRENADE = new ItemPumpkinGrenade("pumpkingrenade");
+	public static final CommonDeferredRegister<Item> ITEMS = CommonDeferredRegister.create(ForgeRegistries.ITEMS, HalloweenLuckyBlockMod.MODID);
 	
-	public static final Item KILLERKNIFE = new ItemKillerKnive(HalloweenLuckyBlockToolMaterials.KILLERKNIFE, "killerknive");
+	public static final RegistryObject<Item> CANDY = ITEMS.register("candy", ItemCandy::new);
+	public static final RegistryObject<Item> TELEPORTER = ITEMS.register("teleporter", ItemTeleporter::new);
+	public static final RegistryObject<Item> CANDYBAG = ITEMS.register("candybag", () -> new ItemCandyBag(false));
+	public static final RegistryObject<Item> CANDYBAG_OP = ITEMS.register("candybag_op", () -> new ItemCandyBag(true));
+	public static final RegistryObject<Item> WITCHBROOMSTICK = ITEMS.register("witchsbroomstick", ItemWitchsBroomstick::new);
+	public static final RegistryObject<Item> PUMPKINGRENADE = ITEMS.register("pumpkingrenade", ItemPumpkinGrenade::new);
 	
-	public static final ArmorSet SCARECROW_SET = ArmorSetCreator.create("scarecrow", HalloweenLuckyBlockItemGroups.GROUP, new Properties(), HalloweenLuckyBlockArmorMaterials.SCARECROW);
-	public static final ArmorSet SLENDER_SET = ArmorSetCreator.create("slender", HalloweenLuckyBlockItemGroups.GROUP, new Properties(), HalloweenLuckyBlockArmorMaterials.SLENDER);
-	public static final ArmorSet WITCH_SET = ArmorSetCreator.create("witch", HalloweenLuckyBlockItemGroups.GROUP, new Properties(), HalloweenLuckyBlockArmorMaterials.WITCH);
-	public static final ArmorSet ZOMBIE_SET = ArmorSetCreator.create("zombie", HalloweenLuckyBlockItemGroups.GROUP, new Properties(), HalloweenLuckyBlockArmorMaterials.ZOMBIE);
-	public static final ArmorSet CLOWN_SET = ArmorSetCreator.create("clown", HalloweenLuckyBlockItemGroups.GROUP, new Properties(), HalloweenLuckyBlockArmorMaterials.CLOWN);
+	public static final RegistryObject<Item> KILLERKNIFE = ITEMS.register("killerknive", () -> new ItemKillerKnive(HalloweenLuckyBlockToolMaterials.KILLERKNIFE));
 	
-	public static final SpawnEggItem CREEPY_ZOMBIE_SPAWN_EGG = new USpawnEggItem("creepy_zombie_spawn_egg", HalloweenLuckyBlockEntityTypes.CREEPY_ZOMBIE, 0x2ecc71, 0xe67e22, new Properties().group(HalloweenLuckyBlockItemGroups.GROUP));
-	public static final SpawnEggItem VAMPIRE_SPAWN_EGG = new USpawnEggItem("vampire_spawn_egg", HalloweenLuckyBlockEntityTypes.VAMPIRE, 0x686c72, 0x000000, new Properties().group(HalloweenLuckyBlockItemGroups.GROUP));
-	public static final SpawnEggItem GHOST_SPAWN_EGG = new USpawnEggItem("ghost_spawn_egg", HalloweenLuckyBlockEntityTypes.GHOST, 0x797a74, 0xffffff, new Properties().group(HalloweenLuckyBlockItemGroups.GROUP));
+	public static final ArmorSet SCARECROW_SET = ArmorSetCreator.create(ITEMS, "scarecrow", HalloweenLuckyBlockItemGroups.GROUP, new Properties(), HalloweenLuckyBlockArmorMaterials.SCARECROW);
+	public static final ArmorSet SLENDER_SET = ArmorSetCreator.create(ITEMS, "slender", HalloweenLuckyBlockItemGroups.GROUP, new Properties(), HalloweenLuckyBlockArmorMaterials.SLENDER);
+	public static final ArmorSet WITCH_SET = ArmorSetCreator.create(ITEMS, "witch", HalloweenLuckyBlockItemGroups.GROUP, new Properties(), HalloweenLuckyBlockArmorMaterials.WITCH);
+	public static final ArmorSet ZOMBIE_SET = ArmorSetCreator.create(ITEMS, "zombie", HalloweenLuckyBlockItemGroups.GROUP, new Properties(), HalloweenLuckyBlockArmorMaterials.ZOMBIE);
+	public static final ArmorSet CLOWN_SET = ArmorSetCreator.create(ITEMS, "clown", HalloweenLuckyBlockItemGroups.GROUP, new Properties(), HalloweenLuckyBlockArmorMaterials.CLOWN);
 	
-	@SubscribeEvent
-	public static void register(Register<Item> event) {
-		BaseRegistryUtil.getAllRegistryEntriesAndApplyNames(HalloweenLuckyBlockMod.MODID, Item.class).forEach(event.getRegistry()::register);
+	public static final RegistryObject<SpawnEggItem> CREEPY_ZOMBIE_SPAWN_EGG = ITEMS.register("creepy_zombie_spawn_egg", () -> new LazySpawnEggItem(HalloweenLuckyBlockEntityTypes.CREEPY_ZOMBIE, 0x2ecc71, 0xe67e22, new Properties().group(HalloweenLuckyBlockItemGroups.GROUP)));
+	public static final RegistryObject<SpawnEggItem> VAMPIRE_SPAWN_EGG = ITEMS.register("vampire_spawn_egg", () -> new LazySpawnEggItem(HalloweenLuckyBlockEntityTypes.VAMPIRE, 0x686c72, 0x000000, new Properties().group(HalloweenLuckyBlockItemGroups.GROUP)));
+	public static final RegistryObject<SpawnEggItem> GHOST_SPAWN_EGG = ITEMS.register("ghost_spawn_egg", () -> new LazySpawnEggItem(HalloweenLuckyBlockEntityTypes.GHOST, 0x797a74, 0xffffff, new Properties().group(HalloweenLuckyBlockItemGroups.GROUP)));
+	
+	public static void registerMod(IEventBus bus) {
+		ITEMS.register(bus);
 	}
 	
 }
--- src/main/java/info/u_team/halloween_luckyblock/util/LazySpawnEggItem.java
+++ src/main/java/info/u_team/halloween_luckyblock/util/LazySpawnEggItem.java
@@ -1,13 +1,24 @@
 package info.u_team.halloween_luckyblock.util;
 
+import java.util.Map;
+import java.util.function.Supplier;
+
+import com.google.common.collect.Maps;
+
 import net.minecraft.entity.EntityType;
-import net.minecraft.item.SpawnEggItem;
+import net.minecraft.item.*;
 
 public class LazySpawnEggItem extends SpawnEggItem {
 	
-	public LazySpawnEggItem(EntityType<?> typeIn, int primaryColorIn, int secondaryColorIn, Properties builder) {
-		super(typeIn, primaryColorIn, secondaryColorIn, builder);
+	private static final Map<Supplier<? extends EntityType<?>>, LazySpawnEggItem> LAZY_EGGS = Maps.newIdentityHashMap();
+	
+	public LazySpawnEggItem(Supplier<? extends EntityType<?>> entityType, int primaryColor, int secondaryColor, Properties builder) {
+		super(null, primaryColor, secondaryColor, builder);
+		LAZY_EGGS.put(entityType, this);
 	}
-
+	
+	public static Map<Supplier<? extends EntityType<?>>, LazySpawnEggItem> getLazyEggs() {
+		return LAZY_EGGS;
+	}
 	
 }
