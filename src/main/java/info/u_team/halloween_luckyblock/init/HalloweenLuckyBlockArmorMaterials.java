15335a85bedb131d7a6079277a2fbf4717ba6ac5
HyCraftHD <hycrafthd@live.de>
2020-10-27 12:18:32 +0100

    Fix armor material

--- src/main/java/info/u_team/halloween_luckyblock/init/HalloweenLuckyBlockArmorMaterials.java
+++ src/main/java/info/u_team/halloween_luckyblock/init/HalloweenLuckyBlockArmorMaterials.java
@@ -7,10 +7,10 @@ import net.minecraft.util.SoundEvents;
 
 public class HalloweenLuckyBlockArmorMaterials {
 	
-	public static final IArmorMaterial SCARECROW = new UArmorMaterialVanilla(200, new int[] { 3, 8, 6, 3 }, 20, SoundEvents.ITEM_ARMOR_EQUIP_GOLD, 0, () -> Ingredient.EMPTY);
-	public static final IArmorMaterial SLENDER = new UArmorMaterialVanilla(200, new int[] { 6, 16, 12, 6 }, 20, SoundEvents.ITEM_ARMOR_EQUIP_GOLD, 0, () -> Ingredient.EMPTY);
-	public static final IArmorMaterial WITCH = new UArmorMaterialVanilla(200, new int[] { 3, 10, 6, 1 }, 20, SoundEvents.ITEM_ARMOR_EQUIP_GOLD, 0, () -> Ingredient.EMPTY);
-	public static final IArmorMaterial ZOMBIE = new UArmorMaterialVanilla(200, new int[] { 2, 8, 7, 3 }, 20, SoundEvents.ITEM_ARMOR_EQUIP_GOLD, 0, () -> Ingredient.EMPTY);
-	public static final IArmorMaterial CLOWN = new UArmorMaterialVanilla(50, new int[] { 20, 20, 20, 20 }, 20, SoundEvents.ITEM_ARMOR_EQUIP_GOLD, 0, () -> Ingredient.EMPTY);
+	public static final IArmorMaterial SCARECROW = new UArmorMaterialVanilla(200, new int[] { 3, 8, 6, 3 }, 20, () -> SoundEvents.ITEM_ARMOR_EQUIP_GOLD, 0, 0, () -> Ingredient.EMPTY);
+	public static final IArmorMaterial SLENDER = new UArmorMaterialVanilla(200, new int[] { 6, 16, 12, 6 }, 20, () -> SoundEvents.ITEM_ARMOR_EQUIP_GOLD, 0, 0, () -> Ingredient.EMPTY);
+	public static final IArmorMaterial WITCH = new UArmorMaterialVanilla(200, new int[] { 3, 10, 6, 1 }, 20, () -> SoundEvents.ITEM_ARMOR_EQUIP_GOLD, 0, 0, () -> Ingredient.EMPTY);
+	public static final IArmorMaterial ZOMBIE = new UArmorMaterialVanilla(200, new int[] { 2, 8, 7, 3 }, 20, () -> SoundEvents.ITEM_ARMOR_EQUIP_GOLD, 0, 0, () -> Ingredient.EMPTY);
+	public static final IArmorMaterial CLOWN = new UArmorMaterialVanilla(50, new int[] { 20, 20, 20, 20 }, 20, () -> SoundEvents.ITEM_ARMOR_EQUIP_GOLD, 0, 0, () -> Ingredient.EMPTY);
 	
 }
