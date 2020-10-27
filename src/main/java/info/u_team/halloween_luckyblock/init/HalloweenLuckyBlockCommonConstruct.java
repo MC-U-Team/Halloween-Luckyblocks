b91e56c96e2a6a1446b0f9a3938e5890820a87c1
HyCraftHD <hycrafthd@live.de>
2020-10-27 12:22:07 +0100

    Register the blocks

--- src/main/java/info/u_team/halloween_luckyblock/init/HalloweenLuckyBlockCommonConstruct.java
+++ src/main/java/info/u_team/halloween_luckyblock/init/HalloweenLuckyBlockCommonConstruct.java
@@ -2,13 +2,14 @@ package info.u_team.halloween_luckyblock.init;
 
 import info.u_team.halloween_luckyblock.HalloweenLuckyBlockMod;
 import info.u_team.u_team_core.api.construct.*;
+import info.u_team.u_team_core.util.registry.BusRegister;
 
 @Construct(modid = HalloweenLuckyBlockMod.MODID)
 public class HalloweenLuckyBlockCommonConstruct implements IModConstruct {
 	
 	@Override
 	public void construct() {
-		
+		BusRegister.registerMod(HalloweenLuckyBlockBlocks::registerMod);
 	}
 	
 }
