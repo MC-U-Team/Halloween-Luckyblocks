d86a2aced4ac7aa8b63896922ab42ecb6f4b17d1
HyCraftHD <hycrafthd@live.de>
2020-10-27 12:13:44 +0100

    Add construct classes

---
+++ src/main/java/info/u_team/halloween_luckyblock/init/HalloweenLuckyBlockClientConstruct.java
@@ -0,0 +1,14 @@
+package info.u_team.halloween_luckyblock.init;
+
+import info.u_team.halloween_luckyblock.HalloweenLuckyBlockMod;
+import info.u_team.u_team_core.api.construct.*;
+
+@Construct(modid = HalloweenLuckyBlockMod.MODID, client = true)
+public class HalloweenLuckyBlockClientConstruct implements IModConstruct {
+	
+	@Override
+	public void construct() {
+		
+	}
+	
+}
---
+++ src/main/java/info/u_team/halloween_luckyblock/init/HalloweenLuckyBlockCommonConstruct.java
@@ -0,0 +1,14 @@
+package info.u_team.halloween_luckyblock.init;
+
+import info.u_team.halloween_luckyblock.HalloweenLuckyBlockMod;
+import info.u_team.u_team_core.api.construct.*;
+
+@Construct(modid = HalloweenLuckyBlockMod.MODID)
+public class HalloweenLuckyBlockCommonConstruct implements IModConstruct {
+	
+	@Override
+	public void construct() {
+		
+	}
+	
+}
