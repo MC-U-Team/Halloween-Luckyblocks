8a4765bea60e39c8f007a373024445912a0ee87c
HyCraftHD <hycrafthd@live.de>
2020-10-27 22:43:16 +0100

    Rename to lazy spawnegg

---
+++ src/main/java/info/u_team/halloween_luckyblock/util/LazySpawnEggItem.java
@@ -0,0 +1,13 @@
+package info.u_team.halloween_luckyblock.util;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.item.SpawnEggItem;
+
+public class LazySpawnEggItem extends SpawnEggItem {
+	
+	public LazySpawnEggItem(EntityType<?> typeIn, int primaryColorIn, int secondaryColorIn, Properties builder) {
+		super(typeIn, primaryColorIn, secondaryColorIn, builder);
+	}
+
+	
+}
