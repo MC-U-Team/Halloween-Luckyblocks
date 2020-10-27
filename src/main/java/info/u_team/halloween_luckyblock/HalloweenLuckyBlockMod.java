5df30f4dfebb3f99897a8833efe795181fdd99b2
HyCraftHD <hycrafthd@live.de>
2020-10-27 12:06:28 +0100

    Add call to jar sign verifier and call annotations

--- src/main/java/info/u_team/halloween_luckyblock/HalloweenLuckyBlockMod.java
+++ src/main/java/info/u_team/halloween_luckyblock/HalloweenLuckyBlockMod.java
@@ -1,10 +1,18 @@
 package info.u_team.halloween_luckyblock;
 
+import info.u_team.u_team_core.util.annotation.AnnotationManager;
+import info.u_team.u_team_core.util.verify.JarSignVerifier;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(HalloweenLuckyBlockMod.MODID)
 public class HalloweenLuckyBlockMod {
 	
 	public static final String MODID = "halloween_luckyblock";
+	
+	public HalloweenLuckyBlockMod() {
+		JarSignVerifier.checkSigned(MODID);
+		
+		AnnotationManager.callAnnotations(MODID);
+	}
 	
 }
