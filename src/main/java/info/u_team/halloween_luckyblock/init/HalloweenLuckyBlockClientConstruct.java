523527a1f6595bd2e22ad24655985407aef7dee6
HyCraftHD <hycrafthd@live.de>
2020-10-31 14:23:46 +0100

    Register renderer a bit diffrenzt

--- src/main/java/info/u_team/halloween_luckyblock/init/HalloweenLuckyBlockClientConstruct.java
+++ src/main/java/info/u_team/halloween_luckyblock/init/HalloweenLuckyBlockClientConstruct.java
@@ -2,13 +2,14 @@ package info.u_team.halloween_luckyblock.init;
 
 import info.u_team.halloween_luckyblock.HalloweenLuckyBlockMod;
 import info.u_team.u_team_core.api.construct.*;
+import info.u_team.u_team_core.util.registry.BusRegister;
 
 @Construct(modid = HalloweenLuckyBlockMod.MODID, client = true)
 public class HalloweenLuckyBlockClientConstruct implements IModConstruct {
 	
 	@Override
 	public void construct() {
-		
+		BusRegister.registerMod(HalloweenLuckyBlockRenderers::registerMod);
 	}
 	
 }
--- src/main/java/info/u_team/halloween_luckyblock/init/HalloweenLuckyBlockRenderers.java
+++ src/main/java/info/u_team/halloween_luckyblock/init/HalloweenLuckyBlockRenderers.java
@@ -1,27 +1,25 @@
 package info.u_team.halloween_luckyblock.init;
 
-import info.u_team.halloween_luckyblock.HalloweenLuckyBlockMod;
 import info.u_team.halloween_luckyblock.entity.render.*;
 import info.u_team.u_team_core.util.registry.ClientRegistry;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.SpriteRenderer;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
-@EventBusSubscriber(modid = HalloweenLuckyBlockMod.MODID, bus = Bus.MOD, value = Dist.CLIENT)
 public class HalloweenLuckyBlockRenderers {
 	
-	@SubscribeEvent
-	public static void register(FMLClientSetupEvent event) {
+	private static void setup(FMLClientSetupEvent event) {
 		ClientRegistry.registerEntityRenderer(HalloweenLuckyBlockEntityTypes.CREEPY_ZOMBIE, RenderCreepyZombie::new);
 		ClientRegistry.registerEntityRenderer(HalloweenLuckyBlockEntityTypes.GHOST, RenderGhost::new);
 		ClientRegistry.registerEntityRenderer(HalloweenLuckyBlockEntityTypes.VAMPIRE, RenderVampire::new);
 		ClientRegistry.registerEntityRenderer(HalloweenLuckyBlockEntityTypes.PUMPKIN_GRENADE, manager -> {
 			return new SpriteRenderer<>(manager, Minecraft.getInstance().getItemRenderer());
 		});
+	}
+	
+	public static void registerMod(IEventBus bus) {
+		bus.addListener(HalloweenLuckyBlockRenderers::setup);
 	}
 	
 }
